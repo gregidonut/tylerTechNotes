ab4b3edd2063679078b958f1b36bcdf6162ed4c1
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-09-30 11:43:36 +0800

    ref: move the tickets api to tickets/get
    since i want to use different files for each method

--- packages/frontend/src/pages/[tenant]/tickets/_components/react/ticketList/queryFn.ts
+++ packages/frontend/src/pages/[tenant]/tickets/_components/react/ticketList/queryFn.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function (): Promise<Array<any>> {
     const { data } = await axios({
         method: "get",
-        url: "/api/tickets",
+        url: "/api/tickets/get",
     });
     return data;
 }
